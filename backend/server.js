0ffa1a8ffdceeec0e2c59e3a0b8e68864f80120b
Jayanth <kolleparajayanth@gmail.com>
2025-01-29 14:32:20 +0530

    dm

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+
+const { connect, connection } = require("mongoose")
+const{app} = require("./app")
+require("dotenv").config()
+
+const port=process.env.PORT
+
+const Connect=require("./db/connection")
+
+app.get("/test",async(req,res)=>{
+
+    res.send("hello.....")
+})
+
+app.listen(port,async()=>{
+    try{
+        await Connect
+        console.log(`app is running on http://localhost:${port}`)
+    }
+    catch(err){
+        console.log(err)
+    }
+    
+ })
